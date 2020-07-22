880b13274732ed558af2ab82480d8ee707731bc4
Minh Vu <vuhoangminh97@gmail.com>
2020-07-22 22:56:51 +0200

    Update taxi_fare_calculation.py

--- taxi_fare_calculation.py
+++ taxi_fare_calculation.py
@@ -108,7 +108,7 @@ history = model.fit(
     train_dataset, train_labels,
     epochs = 1000, batch_size = 128, validation_split = 0.2, verbose = 1,
     callbacks = [keras.callbacks.ReduceLROnPlateau(monitor='val_loss', 
-                                            patience=20, 
+                                            patience=10, 
                                             verbose=1, 
                                             factor=0.6, 
                                             min_lr=0.00005)],
